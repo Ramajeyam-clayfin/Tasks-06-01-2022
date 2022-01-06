981272ae5e829f5c01b6c5c71683de0080bba6ab
Ramajeyam-clayfin <97226564+Ramajeyam-clayfin@users.noreply.github.com>
2022-01-06 18:59:21 +0530

    Import Export ES6 JS

---
+++ export.js
@@ -0,0 +1,17 @@
+function add(a,b){
+	return a+b;
+}
+
+function welcomeUser(name)
+{
+console.log('welcome '+name+" !");
+}
+
+export {add, welcomeUser};
+
+
+
+
+
+
+
---
+++ import.js
@@ -0,0 +1,7 @@
+import {add, welcomeUser} from './export.js';
+var res=add(5,2);
+console.log(res);
+
+welcomeUser("Jeyam");
+
+
